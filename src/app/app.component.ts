1fe8768ca87e627f3265c0b45625485fd77880fc
nemwiz <s3ptease@yahoo.com>
2019-06-13 06:28:46 +0200

    POC for moving panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  elementHeightArea = 90;
+  movingPanelHeight = 200;
+
+
+  movePanel(event: MouseEvent) {
+    const maxMarginFromTop = (this.findOptionIndex(event.y) - 1) * this.elementHeightArea;
+    const panelPosition = maxMarginFromTop - (this.movingPanelHeight / 2) + (this.elementHeightArea / 2);
+
+    document.getElementById('moving-panel').style.marginTop = `${panelPosition.toString()}px`;
+    document.getElementById('moving-panel').style.transition = 'margin 700ms';
+  }
+
+
+  findOptionIndex(yCoord: number) {
+
+
+    for (let i = 0; i < 8; i++) {
+      if (yCoord > i && yCoord < this.elementHeightArea * (i + 1)) {
+        return i + 1;
+      }
+    }
+
+  }
+
 }
